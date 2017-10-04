120a6b3b6e44884c436185d4c4b8f64361ac4a93
Planck1043 <zhao1036713209@gmail.com>
2017-10-04 20:58:40 +0800

    16 array-sum 第二种解法

--- 16-array-sum.rb
+++ 16-array-sum.rb
@@ -1,7 +1,15 @@
 # 给定一阵列内含数字，输出最大值
 
 def find_max(array)
-  array.max
+  m = 0
+
+  array.each do |i|
+    if m < i
+      m = i
+    end
+  end
+
+  return m
 end
 
 arr = [8, 12, 36, 53, 9, 75, 3, 71, 59, 88]
